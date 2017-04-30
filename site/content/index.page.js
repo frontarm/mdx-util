2d457b56d2baac458c1b12e31b4d53281d28ca4b
jamesknelson <james@jamesknelson.com>
2017-05-01 01:23:53 +0900

    meta data for site

--- site/content/index.page.js
+++ site/content/index.page.js
@@ -1,6 +1,8 @@
 export default {
   wrapper: 'SiteWrapper',
   title: "MDXC",
+  metaTitle: "MDXC: Use React Components from Markdown",
+  metaDescription: "MDXC is a tool to convert Markdown into React Components, making it possible to import and use React Components within your Markdown!",
   content: require('./home.md'),
   children: [
     require('../../examples/props.mdx'),
