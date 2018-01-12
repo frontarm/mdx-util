9b2dab28f20a3d47587758820712bcbba2878884
jamesknelson <james@jamesknelson.com>
2018-01-12 20:01:47 +0900

    release mdxc

--- site/loaders/sitepack-raw-page-loader.js
+++ site/loaders/sitepack-raw-page-loader.js
@@ -5,7 +5,7 @@ const frontMatter = require('front-matter')
 const { loadPageWithContent } = require('sitepack/lib/loaderUtils')
 
 
-module.exports = function markdownLoader(content) {
+module.exports = function rawPageLoader(content) {
   const loaderOptions = loaderUtils.getOptions(this) || {};
   const data = frontMatter(content);
   return loadPageWithContent(this, loaderOptions, data.attributes, 'module.exports = '+JSON.stringify(data.body))
